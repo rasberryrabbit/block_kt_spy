49f004a316841c546584e9558d2afb9a76961a08
rasberryrabbit <lovelust@hanmail.net>
2019-08-20 23:43:36 +0900

    Fix loading global setting.

--- content.js
+++ content.js
@@ -1,46 +1,54 @@
-
-var defaultip='59.4.85.230';
-var elements = document.getElementsByTagName('script');
+var defaultip="59.4.85.230";
 var ipchecktext=defaultip;
+  
+function getValue(callback) {
+  chrome.storage.sync.get(['AntiKTIP'], callback);
+}
 
-  ipchecktext = window.localStorage.getItem('AntiKT_IP');
+getValue(function (value) {
+  ipchecktext=value.AntiKTIP;
   if(ipchecktext==null) {
     ipchecktext=defaultip;
   }
+  console.log(ipchecktext);
+  doPatchURL();
+});
 
-iptext = String(ipchecktext).replace(/\./gi,"\\\.");
-ipregex = new RegExp("http\:\/\/"+iptext+"\/tm\/[^\<]+","gi");
-console.log(ipregex);
+function doPatchURL() {
+    var iptext = String(ipchecktext).replace(/\./gi,"\\\.");
+    var ipregex = new RegExp("http\:\/\/"+iptext+"\/tm\/[^\<]+","gi");
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+    var elements = document.getElementsByTagName('script');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(ipregex, document.URL+"?\"");
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
+                var replacedText = text.replace(ipregex, document.URL+"?\"");
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-                console.log(element);
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                    console.log(element);
+                }
             }
         }
     }
-}
 
-var elements = document.getElementsByTagName('iframe');
+    var elements = document.getElementsByTagName('iframe');
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    var text=element.getAttribute("src");
-    if(text!=null) {
-        var replacedText = text.replace(ipregex, document.URL);
-        
-        if (replacedText !== text) {
-          element.setAttribute("src",replacedText);
-          console.log(element);
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var text=element.getAttribute("src");
+        if(text!=null) {
+            var replacedText = text.replace(ipregex, document.URL);
+            
+            if (replacedText !== text) {
+              element.setAttribute("src",replacedText);
+              console.log(element);
+            }
         }
     }
 }
--- options.js
+++ options.js
@@ -1,19 +1,24 @@
 var ipchecktext;
-
 // Saves options to chrome.storage
 function save_options() {
-  var ipchecktext = document.getElementById('iptext').value;
-  window.localStorage.setItem('AntiKT_IP',ipchecktext);
+  ipchecktext = document.getElementById('iptext').value;
+  chrome.storage.sync.set({ 'AntiKTIP': ipchecktext });
   console.log(ipchecktext);
 }
 
+function getValue(callback) {
+  chrome.storage.sync.get(['AntiKTIP'], callback);
+}
+
 function restore_options() {
-  ipchecktext = window.localStorage.getItem('AntiKT_IP');
-  if(ipchecktext==null) {
-    ipchecktext='59.4.85.230';
-  }
-  document.getElementById('iptext').value=ipchecktext;
-  console.log(ipchecktext);
+  getValue(function (value) {
+    ipchecktext=value.AntiKTIP;
+    if(ipchecktext==null) {
+      ipchecktext='59.4.85.230';
+    }
+    document.getElementById('iptext').value=ipchecktext;
+    console.log(ipchecktext);
+  });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
